aa2abbc4abba56e2a72eddd817b95e828fb5be65
arturpereira12 <artur.pereira@academico.ufpb.br>
2025-04-18 18:52:10 -0300

    Remove IncompleteTeamException class

--- src/main/java/br/ufpb/poo/brasileirao/exception/IncompleteTeamException.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.poo.brasileirao.exception;
-
-public class IncompleteTeamException extends RuntimeException {
-    public IncompleteTeamException(String message) {
-        super(message);
-    }
-}
